5b4da75e4c91857d71b967e7d9da80c045a7b871
bhawiyuga <adhitya.bhawiyuga@gmail.com>
2017-05-31 10:55:54 +0700

    websocket

---
+++ kelas-b/11-websocket/simple_websocket_server.py
@@ -0,0 +1,26 @@
+from twisted.internet import protocol, reactor
+from txws import WebSocketFactory
+
+# Definisikan class untuk protocol yg akan kita buat
+class EchoServer(protocol.Protocol) :
+	# Callback func ketika koneksi berhasil dibuat
+	def connectionMade(self) :
+		print "Koneksi baru berhasil dibuat"
+
+	# Callback func ketika ada message masuk dari client
+	def dataReceived(self, data):
+		print data
+		data = "OK "+data
+		# Kirim balik ke client
+		self.transport.write(data)
+
+class EchoFactory(protocol.Factory):
+	def buildProtocol(self, address) :
+		return EchoServer()
+
+factory = EchoFactory()
+reactor.listenTCP(8877, WebSocketFactory(factory) )
+reactor.run()
+
+
+
---
+++ kelas-e/11-websocket/simple_websocket_server.py
@@ -0,0 +1,27 @@
+from twisted.internet import reactor, protocol
+from txws import WebSocketFactory
+
+# Buat class untuk protocol yang kita definisikan
+class EchoServer(protocol.Protocol) :
+	# Definisikan callback func.
+
+	# Callback ketika koneksi berhasil dibuat
+	def connectionMade(self):
+		print "Satu koneksi baru ke client berhasil dibuat"
+
+	# Callback ketika message dari client masuk
+	def dataReceived(self, data) :
+		print data
+		data = "OK "+data
+		# Kirim balik ke client pakai Transport
+		self.transport.write(data)
+
+# Buat class Factory baru
+class EchoFactory(protocol.Factory) :
+	def buildProtocol(self, address) :
+		return EchoServer()
+
+# Inisiasi reactor untuk looping
+factory = EchoFactory()
+reactor.listenTCP(7777, WebSocketFactory(factory))
+reactor.run()
